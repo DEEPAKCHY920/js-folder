4b1894ef8037dd4d0f55df387668164068744e00
DEEPAK CHOUDHARY <technicaldeepak15@gmail.com>
2026-08-20 21:53:00 +0530

    feat: comment out operator examples, add type conversion logic to day2.js, and implement loop and function examples in loops.js

--- day2.js
+++ day2.js
@@ -24,20 +24,42 @@
 //  console.log(result,"\n",selected,"\n",typeof result,"\n",typeof selected);
 
 //  operetaor
- let src=20;
- src+=50;
- console.log(src);
- src-=50;
- console.log(src);
- src*=50;
- console.log(src);
- src/=50;
- console.log(src);
- src%=50;
- console.log(src);
- // comperison operetaor
- console.log(5=="5");
- console.log(5===5);
- console.log(5==="5");
- console.log(5!="5");
- console.log(5!==5);
+//  let src=20;
+//  src+=50;
+//  console.log(src);
+//  src-=50;
+//  console.log(src);
+//  src*=50;
+//  console.log(src);
+//  src/=50;
+//  console.log(src);
+//  src%=50;
+//  console.log(src);
+//  // comperison operetaor
+//  console.log(5=="5");
+//  console.log(5===5);
+//  console.log(5==="5");
+//  console.log(5!="5");
+//  console.log(5!==5);
+// // Logical Operators
+// let age = 29;       // Truthy value (non-zero number)
+// let hasid = true;   // Boolean true
+
+// // Logical AND (&&): Evaluates left-to-right. If the first value is truthy, it returns the second value.
+// // Since age (29) is truthy, it returns hasid (true).
+// console.log(age && hasid); // Outputs: true
+// console.log(age>=18|| hasid);
+// // Logical NOT (!): Negates the boolean value.
+// // Since hasid is true, !hasid evaluates to false.
+// console.log(!hasid); // Outputs: false
+// ternarys opertater 
+// let marks =82;
+// let result= marks>=50 ? "PASS" : "FAIL";
+// console.log(result)
+
+// type conversion
+let values ="10";
+let result= values+5;
+console.log(result);
+console.log(typeof result);
+// number-string 
---
+++ loops.js
@@ -0,0 +1,18 @@
+// Print numbers from 1 to 10 using a for loop.
+for(let i=1;i<=10;i++){
+    console.log(i); // print i number 
+}
+//Print numbers from 10 to 1.
+for(let i=10;i>=1;i--){
+    console.log(i); // print i number 
+}
+function loops(n){
+    for(let i=1;i>=n;i--){
+    console.log(i); // print i number 
+}
+}
+loops(10);
+ function greet(){
+ console.log("hello my dear friend")
+ }
+ greet()
