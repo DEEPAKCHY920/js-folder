72cf49349f783e5b8cbc7048f9e0ed15c1f47584
DEEPAK CHOUDHARY <technicaldeepak15@gmail.com>
2026-08-20 15:31:51 +0530

    feat: add day2.js and first.js files and fix integer formatting in STUDENT.JS

---
+++ day2.js
@@ -0,0 +1,20 @@
+// var city="delhi";
+// console.log(city);
+// let random_no= Math.random();
+// console.log(random_no);
+// const n=96;
+// console.log(n);
+// const  num =3;
+// console.log(num)
+// string
+let student_name= "deepak";
+let course ="bca";
+console.log(student_name,"  ",course);
+console.log(typeof student_name);
+// number
+let num=85.52;
+console.log(typeof num);    
+// bollen
+let ispresent=true;
+console.log(typeof ispresent);
+console.log(ispresent)
---
+++ first.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+// hello this is a comment 
+let name = "John Doe"; // This is a variable declaration
+console.log(name);
+let x =11344;
+let y = 9282;
+console.log(x+y);
+
